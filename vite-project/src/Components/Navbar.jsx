c15923178eb7332db608c6c1739797ca62cf8f3a
DevRamona <r.ingabire@alustudent.com>
2024-07-22 13:54:50 +0200

    Done the navbar

---
+++ vite-project/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div className="flex justify-between bg-purple-600">
+            <div className="flex">
+            <img src="./images/meme.jpeg"></img>
+            <h3>Meme generator</h3>
+
+            </div>
+            <div>
+                <h3>React course - Project3</h3>
+            </div>
+            
+        </div>
+
+    )
+}
